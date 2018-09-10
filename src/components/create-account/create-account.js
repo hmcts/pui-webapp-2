b05eca5bdc8ca3e7013e95b1a1e9a8b5099db0db
Roman Pietrzak <y@ke.mu>
2018-09-10 10:45:01 +0100

    /create-account: all routes added - all views implemented on "dummy" level

--- src/components/create-account/create-account.js
+++ src/components/create-account/create-account.js
@@ -6,6 +6,9 @@ const should = require('should') // eslint-disable-line
         config.routingPrefix
             if it's "/create-account", then all internal component's links
             will be in the form of "/create-account/xxxx/yyyy"
+        config.linkToCreateCasePage
+        config.linkToManageCasePage
+            on the final screen, we render link to "create a case" and "manage a case"
 */
 
 class PUICreateAccountComponent {
@@ -13,6 +16,8 @@ class PUICreateAccountComponent {
         // TODO: some good dev-level checks for config validation needed below
         config.should.have.property('routingPrefix').which.is.a.String()
         config.routingPrefix.length.should.be.above(1)
+        config.should.have.property('linkToCreateCasePage').which.is.a.String()
+        config.should.have.property('linkToManageCasePage').which.is.a.String()
 
         this.config = config
         this.config.routingPrefix = this.config.routingPrefix
@@ -22,6 +27,12 @@ class PUICreateAccountComponent {
         expressApp.get(this.config.routingPrefix, this.routeCreateAccount.bind(this))
         expressApp.get(this.config.routingPrefix + '/organisation-name', this.routeOrganisationName.bind(this))
         expressApp.get(this.config.routingPrefix + '/check-organisation', this.routeCheckOrganisationName.bind(this))
+        expressApp.get(this.config.routingPrefix + '/name', this.routeYourName.bind(this))
+        expressApp.post(this.config.routingPrefix + '/name', this.routeYourName.bind(this)) // TODO: handle /check-organisation POSTed data
+        expressApp.get(this.config.routingPrefix + '/email', this.routeYourEmail.bind(this))
+        expressApp.get(this.config.routingPrefix + '/password', this.routeYourPassword.bind(this))
+        expressApp.get(this.config.routingPrefix + '/check', this.routeCheckYourAnswers.bind(this))
+        expressApp.get(this.config.routingPrefix + '/confirmation', this.routeAcceptAndSend.bind(this))
     }
 
     routeCreateAccount (req, res) {
@@ -49,11 +60,65 @@ class PUICreateAccountComponent {
         let nunjucksVariables = {
             link:
             {
-                //prevPage: this.config.routingPrefix,
-                //nextPage: this.config.routingPrefix + '/check-organisation'
+                prevPage: this.config.routingPrefix + '/organisation-name',
+                nextPage: this.config.routingPrefix + '/name'
             }
         }
         res.render(path.join(__dirname, '/check-organisation'), nunjucksVariables)
+    }
+
+    routeYourName (req, res) {
+        let nunjucksVariables = {
+            link:
+            {
+                prevPage: this.config.routingPrefix + '/check-organisation',
+                nextPage: this.config.routingPrefix + '/email'
+            }
+        }
+        res.render(path.join(__dirname, '/name'), nunjucksVariables)
+    }
+
+    routeYourEmail (req, res) {
+        let nunjucksVariables = {
+            link:
+            {
+                prevPage: this.config.routingPrefix + '/name',
+                nextPage: this.config.routingPrefix + '/password'
+            }
+        }
+        res.render(path.join(__dirname, '/email'), nunjucksVariables)
+    }
+
+    routeYourPassword (req, res) {
+        let nunjucksVariables = {
+            link:
+            {
+                prevPage: this.config.routingPrefix + '/email',
+                nextPage: this.config.routingPrefix + '/check'
+            }
+        }
+        res.render(path.join(__dirname, '/password'), nunjucksVariables)
+    }
+
+    routeCheckYourAnswers (req, res) {
+        let nunjucksVariables = {
+            link:
+            {
+                nextPage: this.config.routingPrefix + '/confirmation'
+            }
+        }
+        res.render(path.join(__dirname, '/check'), nunjucksVariables)
+    }
+
+    routeAcceptAndSend (req, res) {
+        let nunjucksVariables = {
+            link:
+            {
+                createCasePage: this.config.linkToCreateCasePage,
+                manageCasePage: this.config.linkToManageCasePage
+            }
+        }
+        res.render(path.join(__dirname, '/confirmation'), nunjucksVariables)
     }
 }
 
