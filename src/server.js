178a98f768212e43cc9568e5d51acb94e8ece654
alan <alan.work@teamsmog.com>
2018-09-10 14:58:04 +0100

    static assets fix  + prettier

--- src/server.js
+++ src/server.js
@@ -14,44 +14,44 @@ const PORT = 4001
 
 // Read from ENV in prod somehow?
 var config = {
-  sessionSecret: 's3cretSauc3',
-  secureCookie: false // this needs to be 'true' in prod and needs https encryption to be used
+    sessionSecret: 's3cretSauc3',
+    secureCookie: false // this needs to be 'true' in prod and needs https encryption to be used
 }
 
 var viewDirs = [
-  __dirname,
-  'components',
-  'assets/templates',
-  '../node_modules/govuk-frontend/',
-  '../node_modules/govuk-frontend/components',
-  '../node_modules/@hmcts/frontend/components'
+    __dirname,
+    'components',
+    'assets/templates',
+    '../node_modules/govuk-frontend/',
+    '../node_modules/govuk-frontend/components',
+    '../node_modules/@hmcts/frontend/components'
 ]
 
 nunjucks.configure(viewDirs, {
-  autoescape: true,
-  express: app,
-  noCache: true,
-  watch: true
+    autoescape: true,
+    express: app,
+    noCache: true,
+    watch: true
 })
 
 //sessions
 const FileStore = sessionFileStore(session)
 
 app.use(
-  session({
-    cookie: {
-      httpOnly: true,
-      maxAge: 31536000,
-      secure: config.secureCookie
-    },
-    name: 'pui-webapp-2',
-    resave: true,
-    saveUninitialized: true,
-    secret: config.sessionSecret,
-    store: new FileStore({
-      path: process.env.NOW ? `/tmp/sessions` : `.sessions`
+    session({
+        cookie: {
+            httpOnly: true,
+            maxAge: 31536000,
+            secure: config.secureCookie
+        },
+        name: 'pui-webapp-2',
+        resave: true,
+        saveUninitialized: true,
+        secret: config.sessionSecret,
+        store: new FileStore({
+            path: process.env.NOW ? `/tmp/sessions` : `.sessions`
+        })
     })
-  })
 )
 
 //express view engine settings
@@ -61,27 +61,29 @@ app.set('view engine', 'html')
 app.use(express.static('../dist'))
 
 app.use((req, res, next) => {
-  res.setHeader('Cache-Control', 'private, no-cache, no-store, max-age=0')
-  res.setHeader('Pragma', 'no-cache')
-  res.setHeader('Expires', '0')
-  next()
+    res.setHeader('Cache-Control', 'private, no-cache, no-store, max-age=0')
+    res.setHeader('Pragma', 'no-cache')
+    res.setHeader('Expires', '0')
+    next()
 })
 
 // static assets
 app.use(express.static('dist'))
 app.use('/assets', express.static(path.join(__dirname, '../dist/assets')))
+app.use('/assets', express.static(path.join(__dirname, '../node_modules', 'govuk-frontend', 'assets')))
+app.use('/assets', express.static(path.join(__dirname, '../node_modules', '@hmcts', 'frontend', 'assets')))
 
 // components
 app.get('/', homeComponent.home)
 
 let puiCreateAccountComponent = new PUICreateAccountComponent({
-  routingPrefix: '/create-account',
-  linkToCreateCasePage: '/create-case',
-  linkToManageCasePage: '/manage-case'
+    routingPrefix: '/create-account',
+    linkToCreateCasePage: '/create-case',
+    linkToManageCasePage: '/manage-case'
 })
 puiCreateAccountComponent.installToExpress(app)
 
 // Start !
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+    console.log(`listening on port ${PORT}`)
 })
