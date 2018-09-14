21cca292859115c44f19ec4442d1d54f55d9f487
alan <alan.work@teamsmog.com>
2018-09-14 09:43:05 +0100

    tidy up

--- src/ui/home/home.js
+++ src/ui/home/home.js
@@ -4,5 +4,5 @@ module.exports.home = function(req, res) {
     let idam = res.getInstanceOf('idam')
     const idamLabel = idam.isAuthorised(req, res) ? 'Sign Out' : ''
     const idamlink = idam.isAuthorised(req, res) ? idam.config.routingPrefix + '/logout' : ''
-    res.render(path.join(__dirname, '/home'), { idamlabel: idamLabel, idamLink: idam.config.routingPrefix + '/logout' })
+    res.render(path.join(__dirname, '/home'), { idamlabel: idamLabel, idamLink: idamlink })
 }
